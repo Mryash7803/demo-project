0596beecaefa622f0bf1ced118fda871bc34b776
Yash Singar <yashsingar@localhost.localdomain>
2025-10-25 17:12:05 +0530

    for project adding files

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from our CI/CD Pipeline v1!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
